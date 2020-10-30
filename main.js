eb5644c352ed667d2e441a6ee2fc6dd37ce8bd12
Brett Flavin <brettflavin@yahoo.com>
2020-10-29 21:08:20 -0700

    Added keyframe animations and ScrollReveal animations

--- main.js
+++ main.js
@@ -13,3 +13,33 @@ menuToggler.addEventListener('click', () => {
     })
 });
 
+// Scroll Reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1300,
+    distance: '25rem',
+    delay: 100,
+    // scale: .5
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1300,
+    distance: '25rem',
+    delay: 100,
+    // scale: .5
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1100,
+    distance: '1rem'
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '5rem'
+});
